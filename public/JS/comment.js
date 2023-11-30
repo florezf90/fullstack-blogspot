a7999bedde5284811bbc0917536b4f9f23c0b1f0
florezf90 <florezf90@gmail.com>
2023-11-30 02:37:03 -0600

    edited the public folder

--- public/JS/comment.js
+++ public/JS/comment.js
@@ -2,25 +2,20 @@ const commentformHanlder = async (event) => {
   event.preventDefault();
 
   // collect comment content from the comment form
-  const commentContent = document.querySelector("#comment-input").value.trim();
+  const commentContent = document
+    .querySelector("textarea[name='comment-body']")
+    .value.trim();
   //  get the post ID from the URL
-  const currentUrl = window.location.href;
-  const urlParts = currentUrl.split("/");
-  const postId = urlParts[urlParts.length - 1];
-  // creates an object with comment, post ID and User ID
-  const commentData = {
-    content: commentContent,
-    post_id: postId,
-  };
 
-  console.log(commentContent);
-  console.log(postId);
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
 
   if (commentContent) {
     // Send a POST request to the API endpoint
-    const response = await fetch(`/api/comments/post${postId}`, {
+    const response = await fetch(`/api/comments`, {
       method: "POST",
-      body: JSON.stringify({ commentData }),
+      body: JSON.stringify({ post_id, commentContent }),
       headers: { "Content-Type": "application/json" },
     });
 
