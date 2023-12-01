86a751a322e3e59783d8c052a8d79582559ca21e
florezf90 <florezf90@gmail.com>
2023-12-01 01:58:51 -0600

    help

--- controllers/API/index.js
+++ controllers/API/index.js
@@ -1,9 +1,10 @@
 const path = require("path");
 console.log("__dirname:", __dirname);
+const User = require("./User-routes.js");
 
 const router = require("express").Router();
 
-const userRoutes = require("./user-routes.js");
+const User = require("./User-routes.js");
 const postRoutes = require("../API/post-routes.js");
 const commentRoutes = require("../API/comment-routes.js");
 
