a3a2232eb02ed5cdf4efe6779f04f7ab2ccfe55d
florezf90 <florezf90@gmail.com>
2023-12-01 02:14:26 -0600

    fixing

--- controllers/API/index.js
+++ controllers/API/index.js
@@ -1,13 +1,10 @@
-const path = require("path");
-console.log("__dirname:", __dirname);
-
 const router = require("express").Router();
 
-const User = require("./User-routes.js");
-const postRoutes = require("../API/post-routes.js");
-const commentRoutes = require("../API/comment-routes.js");
+const userRoutes = require("./user-routes");
+const postRoutes = require("./post-routes");
+const commentRoutes = require("./comment-routes");
 
-router.use("/users", User);
+router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
 router.use("/comments", commentRoutes);
 
