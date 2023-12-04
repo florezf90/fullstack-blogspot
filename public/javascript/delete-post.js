c09fb8b1f8028f7e87a33896b5958d00f5188bd3
florezf90 <florezf90@gmail.com>
2023-12-03 22:12:05 -0600

    fixed delete feature

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,26 +1,29 @@
 async function deleteFormHandler(event) {
-    event.preventDefault();
-    
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-      ];
+  event.preventDefault();
 
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE',
-        body: JSON.stringify({
-          post_id: id
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-    
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  console.log("Post ID:", id);
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: "DELETE",
+    body: JSON.stringify({
+      id: id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard/");
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+}
+
+document
+  .querySelector(".delete-post-btn")
+  .addEventListener("click", deleteFormHandler);
